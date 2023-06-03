aa0edb6c98cf8066b6a872ad35ffe72678a447da
Yusuf Uzan <yusuf.uzan@kariyer.net>
2023-06-03 13:57:40 +0300

    Service response changed

--- Sources/SwiftPlus/Extension/UIView+.swift
+++ Sources/SwiftPlus/Extension/UIView+.swift
@@ -53,4 +53,18 @@ public extension UIView {
     self.clipsToBounds = false
   }
 
+  @discardableResult
+  func addBlur(style: UIBlurEffect.Style = .extraLight, alpha: CGFloat = 1.0) -> UIVisualEffectView {
+    let blurEffect = UIBlurEffect(style: style)
+    let blurBackground = UIVisualEffectView(effect: blurEffect)
+    addSubview(blurBackground)
+    blurBackground.translatesAutoresizingMaskIntoConstraints = false
+    blurBackground.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+    blurBackground.topAnchor.constraint(equalTo: topAnchor).isActive = true
+    blurBackground.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+    blurBackground.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+    blurBackground.alpha = alpha
+    return blurBackground
+  }
+  
 }
