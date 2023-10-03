723e5a2292f90130a4f0f3461ffaa2110aa8a654
yusuf u <yusuf.uzan@kariyer.net>
2023-10-03 23:08:02 +0300

    SSS files created

--- Sources/SwiftPlus/Extension/UIApplication+.swift
+++ Sources/SwiftPlus/Extension/UIApplication+.swift
@@ -16,5 +16,20 @@ public extension UIApplication {
   class var topInset: CGFloat {
     UIApplication.shared.keyWindow?.safeAreaInsets.top ?? UIApplication.shared.statusBarFrame.size.height
   }
-
+  
+  struct Constants {
+    static let CFBundleShortVersionString = "CFBundleShortVersionString"
+  }
+  class func appVersion() -> String {
+    Bundle.main.object(forInfoDictionaryKey: Constants.CFBundleShortVersionString) as! String
+  }
+  class func appBuild() -> String {
+    Bundle.main.object(forInfoDictionaryKey: kCFBundleVersionKey as String) as! String
+  }
+  
+  class func version() -> String {
+    let version = appVersion(), build = appBuild()
+    return "v\(version) - (\(build))"
+  }
+  
 }
